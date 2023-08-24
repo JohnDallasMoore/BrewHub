a9e4bf36b0c15ba3fca24a4bcfbad4889a4e7c69
Dallas Moore <moore.johndallas@gmail.com>
2023-08-24 17:20:30 -0500

    added login page

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -12,7 +12,7 @@ function NavBar() {
             <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">BrewHub</span>
         </a>
         <div className="flex md:order-2">
-            <button type="button" className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Log In</button>
+            <Link to="/login" className="text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">Log In</Link>
             <button data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm rounded-lg md:hidden  focus:outline-none focus:ring-2 text-gray-400 hover:bg-gray-700 focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
               <span className="sr-only">Open main menu</span>
               <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -2,7 +2,45 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <section class="bg-gray-800">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            
+            <div class="w-full rounded-lg shadow border md:mt-0 sm:max-w-md xl:p-0 bg-gray-900 border-gray-700">
+                <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                    <h1 class="text-xl font-bold leading-tight tracking-tight  md:text-2xl text-white">
+                        Sign in to your account
+                    </h1>
+                    <form class="login-form space-y-4 md:space-y-6" action="#">
+                        <div>
+                            <label for="email" class="block mb-2 text-sm font-medium text-white">Your email</label>
+                            <input type="email" name="email" id="email-login" class=" border  sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="name@brewhub.com" required="" />
+                        </div>
+                        <div>
+                            <label for="password" class="block mb-2 text-sm font-medium text-white">Password</label>
+                            <input type="password" name="password" id="password-login" placeholder="••••••••" class="border sm:text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" required="" />
+                        </div>
+                        <div class="flex items-center justify-between">
+                            <div class="flex items-start">
+                                <div class="flex items-center h-5">
+                                  <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border rounded focus:ring-3 bg-gray-700 border-gray-600 focus:ring-primary-600 ring-offset-gray-800" />
+                                </div>
+                                <div class="ml-3 text-sm">
+                                  <label for="remember" class="text-gray-300">Remember me</label>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <button type="submit" class="w-full text-white focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-primary-800">Sign in</button>
+                        <p class="text-sm font-light text-gray-400">
+                            Don’t have an account yet? <a href="/signup" class="font-medium hover:underline text-blue-500">Sign Up</a>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
