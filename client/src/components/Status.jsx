572d7f83d7a00748cdcc406e5debf0a93ba3e431
Dallas Moore <moore.johndallas@gmail.com>
2023-08-27 14:52:35 -0500

    updated profile page, updated status/review components, hard coded images for styling

--- client/src/components/Status.jsx
+++ client/src/components/Status.jsx
@@ -2,7 +2,44 @@ import React from 'react'
 
 function Status() {
   return (
-    <div>Status</div>
+    <div>
+      <main className="pt-4 pb-8 lg:pt-8 lg:pb-12 bg-gray-700 max-w-lg">
+          <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
+              <article className="border-4 rounded-lg p-8 border-gray-900 bg-gray-800 mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+                  <header className="mb-4 lg:mb-6 not-format">
+                      <address className="flex items-center mb-6 not-italic">
+                          <div className="inline-flex items-center mr-3 text-sm text-white">
+                              <div>
+                                  <a href="#" rel="author" className="text-xl font-bold text-white">Dallas Moore</a>
+                                  <p className="text-base text-sm font-light text-gray-400">August 26, 2023</p>
+                              </div>
+                          </div>
+                      </address>
+                      
+                  </header>
+                  <figure><img className="mx-auto rounded-md object-contain h-96 w-96" src="/Dallas-Status.jpeg" alt="" />
+                  <br />
+                  <p className="text-base font-light text-gray-400">Western Sun IPA from Social Project is a hop lover's paradise. With its robust blend of citrusy and resinous flavors, this IPA shines brightly, leaving a lingering satisfaction on the palate.</p>
+                  </figure>
+                  <br />
+                  <div className="flex items-center">
+                      
+                      <a href="#" className="text-sm font-medium underline hover:no-underline text-white">12 Likes</a>
+                      <span className="w-1 h-1 mx-1.5 rounded-full bg-gray-400"></span>
+                      <button id="comment-button" className="text-sm font-medium underline hover:no-underline text-white comment-buttons">Comments</button>
+                  </div>
+                  <div id="comment-input" className="hidden">
+                      {/* <Comment /> */}
+                  </div>
+                  <p id="post-id" className="text-gray-800 text-opacity-0">
+                      {/* post id for finding if needed */}
+                  </p>
+              </article>
+          </div>
+      </main> 
+    </div>
+
+
   )
 }
 
--- client/src/pages/DashPages/Profile.jsx
+++ client/src/pages/DashPages/Profile.jsx
@@ -1,39 +1,61 @@
 import React from "react";
+import Review from "../../components/Review";
+import Status from "../../components/Status";
 
 function Profile() {
   return (
     <div>
-      <section class="lg:my-4 lg:mx-48 rounded-lg bg-gray-800">
-          <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-              <img class="w-full rounded-full" src="/dallas-pic.jpg" alt="dashboard image" />
-              <div class="mt-4 md:mt-0">
-                  <h2 class="mb-4 text-8xl tracking-tight font-extrabold text-white">Dallas Moore</h2>
-                  <p class="mb-6 font-light md:text-lg text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p>
-                  <a href="#" class="inline-flex items-center text-white bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
-                      Follow
-                      <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                  </a>
-                  <div class="w-full lg:w-full px-4 lg:order-1">
-                    <div class="flex justify-center py-4 lg:pt-4 pt-8">
-                      <div class="mr-4 p-3 text-center">
-                        <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">22</span><span class="text-sm text-blueGray-400">Followers</span>
+      <section className="my-2 lg:my-4 lg:mx-12  rounded-lg bg-gray-800">
+          <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+              <img className="w-full rounded-full" src="/dallas-pic.jpg" alt="dashboard image" />
+              <div className="mt-4 md:mt-0">
+                  <h2 className="mb-4 text-4xl text-center lg:text-8xl tracking-tight font-extrabold text-white">Dallas Moore</h2>
+                  <p className="mb-6 font-light md:text-lg text-center text-gray-400">Sipping through life, one brew at a time 🍻 Beer enthusiast on a journey of flavors and stories. Join me in celebrating the art of brewing and the joy of discovering new beers!</p>
+                  <div className="flex flex-col items-center">
+                    <a href="#" className="inline-flex items-center  text-white bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
+                        Follow
+                        <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+                    </a>
+
+                  </div>
+                  <div className="w-full lg:w-full px-4 lg:order-1">
+                    <div className="flex flex-wrap justify-center py-4 lg:pt-4 pt-8">
+                      <div className="mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">22</span><span className="text-sm text-blueGray-400">Followers</span>
                       </div>
-                      <div class="mr-4 p-3 text-center">
-                        <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">10</span><span class="text-sm text-blueGray-400">Following</span>
+                      <div className="mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">10</span><span className="text-sm text-blueGray-400">Following</span>
                       </div>
-                      <div class="lg:mr-4 p-3 text-center">
-                        <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">12</span><span class="text-sm text-blueGray-400">Reviews</span>
+                      <div className="lg:mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">12</span><span className="text-sm text-blueGray-400">Reviews</span>
                       </div>
-                      <div class="lg:mr-4 p-3 text-center">
-                        <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">18</span><span class="text-sm text-blueGray-400">Statuses</span>
+                      <div className="lg:mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">18</span><span className="text-sm text-blueGray-400">Statuses</span>
                       </div>
-                      <div class="lg:mr-4 p-3 text-center">
-                        <span class="text-xl font-bold block uppercase tracking-wide text-blueGray-600">89</span><span class="text-sm text-blueGray-400">Achievements</span>
+                      <div className="lg:mr-4 p-3 text-center">
+                        <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">89</span><span className="text-sm text-blueGray-400">Achievements</span>
                       </div>
                     </div>
                   </div>
               </div>
           </div>
+      </section>
+      <section className="lg:my-4 lg:mx-12  rounded-xl bg-gray-800">
+        <div className="flex flex-wrap justify-around ">
+          <div className="border-4 rounded-md bg-gray-700 border-gray-900 mx-4 my-8">
+            <h2 className="m-4 text-4xl text-center lg:text-6xl tracking-tight font-extrabold text-white">Your Reviews</h2>
+            <Review />
+            <Review />
+            <Review />
+          </div>
+          <hr />
+          <div className="border-4 rounded-md bg-gray-700 border-gray-900 mx-4 my-8">
+            <h2 className="m-4 text-4xl text-center lg:text-6xl tracking-tight font-extrabold text-white">Your Statuses</h2>
+            <Status />
+            <Status />
+            <Status />
+          </div>
+        </div>
       </section>
       
     </div>
