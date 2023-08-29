6a0b25ae85ce1be1176ac46f935e6a6810d2950d
Dallas Moore <moore.johndallas@gmail.com>
2023-08-28 23:08:55 -0500

    Added printable image container

--- client/src/pages/DashPages/Printables.jsx
+++ client/src/pages/DashPages/Printables.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Printables() {
   return (    
-      <div>
+      <div className=''>
         <section className="border-4 border-gray-900 my-2 lg:my-4 lg:mx-12  rounded-xl bg-gray-800">
             <div className="flex flex-wrap justify-around ">
             <div className="rounded-md  mx-4 my-8">
@@ -11,60 +11,36 @@ function Printables() {
             </div>
             </div>
         </section>
-        <div id="default-carousel" class="relative w-full" data-carousel="slide">
-            {/* <!-- Carousel wrapper --> */}
-            <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="/Beer-Pour.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
-                
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="/Beer-Can.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
-                
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="/Beer-Pour.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
-                
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="/Beer-Can.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
-                
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="/Beer-Pour.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
-                
-                <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                    <img src="/Beer-Can.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
-                </div>
+       
+        <div class="grid grid-cols-2 gap-2 my-2 lg:my-4 lg:mx-12  rounded-xl ">
+            <div>
+                <a href="/printable-1.pdf" download>
+                    <img class="h-auto max-w-full rounded-lg" src="/printable-1.pdf" alt="" />
+                </a>
             </div>
-            {/* <!-- Slider indicators --> */}
-            <div class="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+            <div>
+                <a href="/printable-2.jpg" download>
+                    <img class="h-auto max-w-full rounded-lg" src="/printable-2.jpg" alt="" />
+                </a>
             </div>
-            {/* <!-- Slider controls --> */}
-            <button type="button" class="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                    </svg>
-                    <span class="sr-only">Previous</span>
-                </span>
-            </button>
-            <button type="button" class="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                    <svg class="w-4 h-4 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                    </svg>
-                    <span class="sr-only">Next</span>
-                </span>
-            </button>
+            <div>
+                <a href=""></a>
+                <img class="h-auto max-w-full rounded-lg" src="/printable-3.jpg" alt="" />
+            </div>
+            <div>
+                <a href=""></a>
+                <img class="h-auto max-w-full rounded-lg" src="/printable-4.jpg" alt="" />
+            </div>
+            <div>
+                <a href=""></a>
+                <img class="h-auto max-w-full rounded-lg" src="/printable-5.jpg" alt="" />
+            </div>
+            <div>
+                <a href=""></a>
+                <img class="h-auto max-w-full rounded-lg" src="/printable-6.jpg" alt="" />
+            </div>
         </div>
+
 
       </div>
   )
