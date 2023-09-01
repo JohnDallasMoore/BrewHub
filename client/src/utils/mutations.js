0cff0fc9ba3afddf23a472ca5436e55b3c297624
Dallas Moore <moore.johndallas@gmail.com>
2023-08-31 20:17:12 -0500

    fixed client utils

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,79 +1,96 @@
 import { gql } from '@apollo/client';
 
-export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-                email
-            }
-        }
+export const LOGIN = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        firstName
+        lastName
+        email
+      }
     }
-    `;
+  }
+`;
 
-export const LOGIN_USER = gql`
-    mutation login($email: String!, $password: String!) {
-        login(email: $email, password: $password){
-            token
-            user {
-                _id
-                username
-                email
-            }
-        }
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $password: String!
+    $email: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      password: $password
+      email: $email
+    ) {
+      token
+      user {
+        _id
+        firstName
+        lastName
+        email
+      }
     }
-    `;
+  }
+`;
 
 export const ADD_STATUS = gql`
-    mutation addStatus($statusText: String!) {
-        addStatus(statusText: $statusText) {
-            _id
-            statusText
-            createdAt
-            username
-            commentCount
-            comments {
-                _id
-                createdAt
-                username
-                commentText
-            }
-        }
+  mutation addStatus(
+    $content: String!
+    $image: String!
+    $likes: Int!
+  ) {
+    addStatus(
+      content: $content
+      image: $image
+      likes: $likes
+    ) {
+      _id
+      content
+      image
+      likes
     }
-    `;
+  }
+`;
 
 export const ADD_REVIEW = gql`
-    mutation addReview($reviewText: String!) {
-        addReview(reviewText: $reviewText) {
-            _id
-            reviewText
-            createdAt
-            username
-            commentCount
-            comments {
-                _id
-                createdAt
-                username
-                commentText
-            }
-        }
+  mutation addReview(
+    $title: String!
+    $content: String!
+    $rating: Int!
+    $image: String!
+    $likes: Int!
+  ) {
+    addReview(
+      title: $title
+      content: $content
+      rating: $rating
+      image: $image
+      likes: $likes
+    ) {
+      _id
+      title
+      content
+      rating
+      image
+      likes
     }
-    `;
+  }
+`;
 
 export const ADD_COMMENT = gql`
-    mutation addComment($statusId: ID!, $commentText: String!) {
-        addComment(statusId: $statusId, commentText: $commentText) {
-            _id
-            commentCount
-            comments {
-                _id
-                commentText
-                createdAt
-                username
-            }
-        }
+  mutation addComment(
+    $content: String!
+  ) {
+    addComment(
+      content: $content
+    ) {
+      _id
+      content
     }
-    `;
-
+  }
+`;
