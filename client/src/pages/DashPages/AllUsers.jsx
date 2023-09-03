ae093a1e85354b0ddc2f86f94988ba73a3698f5e
Dallas Moore <moore.johndallas@gmail.com>
2023-09-03 13:58:55 -0500

    fixed signup and adjusted rendering

--- client/src/pages/DashPages/AllUsers.jsx
+++ client/src/pages/DashPages/AllUsers.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import UserCard from '../../components/UserCard'
+import { GET_USERS } from '../../utils/queries'
+import { GET_USER_BY_ID } from '../../utils/queries'
+import { useQuery } from '@apollo/client'
 
 function AllUsers() {
+  const { loading, data } = useQuery(GET_USERS)
+  const users = data?.users || []
   return (
     <div>
       <section className="bg-gray-700">
@@ -11,14 +16,14 @@ function AllUsers() {
                 <p className="font-light text-gray-400 sm:text-xl">Meet the BrewCrew: Explore user profiles, follow, and connect for a glimpse into diverse beer journeys and experiences.</p>
             </div> 
             <div className="grid gap-8 lg:gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-                <UserCard />
-                <UserCard />
-                <UserCard />
-                <UserCard />
-                <UserCard />
-                <UserCard />
-                <UserCard />
-                <UserCard />
+                {loading ? (
+                  <div>Loading...</div>
+                ) : ( users.map((user) => (
+                  <UserCard
+                    user={user}
+                    key={user._id}
+                  />
+                )))}
             </div> 
             <div>
               <a href="#" className="inline-flex items-center mt-12 text-white bg-blue-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900">
--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,25 +1,28 @@
 const db = require('../config/connection');
-
 const { User, Review, Status } = require('../models');
-
-const cleanDB = require('./cleanDB');
-
+const cleanDB = require('./cleanDB.js');
 const userData = require('./userData.json');
-
 const reviewData = require('./reviewData.json');
+const statusData = require('./statusData.json'); // Assuming you have a statusData file
 
 db.once('open', async () => {
+  try {
     await cleanDB("User", "user");
     await cleanDB("Review", "review");
     await cleanDB("Status", "status");
 
-    const users = await User.insertMany(userData);
+    await User.insertMany(userData);
+    await Status.insertMany(statusData);
+    await Review.insertMany(reviewData);
 
-    const statuses = await Status.insertMany(statusData);
-
-    const reviews = await Review.insertMany(reviewData);
-
-    console.log('all done!');
+    console.log('Seed data inserted successfully!');
+  } catch (error) {
+    console.error('Error seeding data:', error);
+  } finally {
     process.exit(0);
+  }
 });
 
+db.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
