3c3cf52a18fdb95f83c6361c48e6daadf5cdb05e
NuminousBlue <jdiehl2236@gmail.com>
2023-09-06 10:11:29 -0500

    refactor Dashboard.jsx for user context

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-hooks/rules-of-hooks */
 import { useContext, createContext, useMemo, useCallback, useState } from "react";
 import { PropTypes } from 'prop-types'
 
@@ -6,12 +5,16 @@ const UserContext = createContext();
 
 function UserContextProvider({ children }) {
     const [user, setUser] = useState({
+        // Any other user-related state can be stored here
         loggedIn: false
     })
 
+    // useCallback so these functions are never needlessly re-computed
+    // https://react.dev/reference/react/useCallback
     const login = useCallback(() => setUser({ loggedIn: true }), [])
     const logout = useCallback(() => setUser({ loggedIn: false }), [])
 
+    // useMemo so the context values are only re-computed when necessary 
     const value = useMemo(() => ({
         login,
         logout,
@@ -23,8 +26,14 @@ function UserContextProvider({ children }) {
     </UserContext.Provider>
 }
 
+/* 
+Custom hook so we don't have to import both useContext and UserContext
+every time we want to use the user context
+*/
 const useUserContext = () => useContext(UserContext);
 
+// Runtime type validation for props
+// No longer used often because TypeScript
 UserContextProvider.propTypes = {
     children: PropTypes.arrayOf(PropTypes.element)
 }
